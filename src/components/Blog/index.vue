a3639ba6eb65a31b370fcdc71b271b2a82a949a9
buraktnv <buraktnv@gmail.com>
2023-07-24 14:31:27 +0300

    hero card yapısı kuruldu

--- src/components/Blog/index.vue
+++ src/components/Blog/index.vue
@@ -1,13 +1,32 @@
 <template>
-    <div class="blog">
-        <h1>This is a blog page.</h1>
-    </div>
+  <div class="blog">
+    <h1>This is a blog page.</h1>
+    <hero-card
+      :image-src="heroCard.imageSrc"
+      :title="heroCard.title"
+      :sub-title="heroCard.subTitle"
+    />
+  </div>
 </template>
 
 <script>
+import HeroCard from "@/components/card/HeroCard.vue";
+
 export default {
-    name: 'Blog',
-}
+  name: "Blog",
+  components: {
+    HeroCard,
+  },
+  data() {
+    return {
+      heroCard: {
+        imageSrc: "https://picsum.photos/seed/picsum/200/300",
+        title: "this is hero title",
+        subTitle: "this is sub title",
+      },
+    };
+  },
+};
 </script>
 
 <style scoped lang="scss" scoped></style>
---
+++ src/components/card/HeroCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card -hero">
+    <img :src="imageSrc" alt="hero card image" />
+    <p class="title">{{ title }}</p>
+    <p class="title -sub">{{ subTitle }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroCard",
+  props: {
+    title: String,
+    subTitle: String,
+    imageSrc: String,
+  },
+};
+</script>
+<style></style>
