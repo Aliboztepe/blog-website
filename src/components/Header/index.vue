82691b08f995f80886d1096d7520f2984e6ae527
Deniz Sarıkaş <75947870+denizsarikas@users.noreply.github.com>
2023-07-25 11:11:32 +0300

    class isimleri düzenlendi

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,29 +1,29 @@
 <template>
-    <header class="header">
-      <div class="header__logo">Nordic Rose</div>
-      <button v-if="isMobile" class="hamburger" @click="toggleMenu">
-        <span v-if="!showMenu">&#9776;</span>
-        <span class="close-hamburger" v-else>&#10006;</span>
-      </button>
-      <nav :class="isMobile && showMenu ? 'menu-mobile' : 'menu-desktop'">
-        <router-link to="/" exact class="nav-link">Home</router-link>
-        <router-link to="/about" class="nav-link">About</router-link>
-        <router-link to="/blog" class="nav-link">Blog</router-link>
-      </nav>
-    </header>
+  <header class="header">
+    <div class="header__logo">Nordic Rose</div>
+    <button v-if="isMobile" class="header__hamburger" @click="toggleMenu">
+      <span v-if="!showMenu">&#9776;</span>
+      <span class="close-hamburger" v-else>&#10006;</span>
+    </button>
+    <nav :class="isMobile && showMenu ? 'header__mobile' : 'header__desktop'">
+      <router-link to="/" exact class="nav-link">Home</router-link>
+      <router-link to="/about" class="nav-link">About</router-link>
+      <router-link to="/blog" class="nav-link">Blog</router-link>
+    </nav>
+  </header>
 </template>
 
 <!-- 
-    checkMobile() {
-  if (window.innerWidth <= 768) {
-    this.isMobile = true;
-  } else {
-    this.isMobile = false;
-  }
+  checkMobile() {
+if (window.innerWidth <= 768) {
+  this.isMobile = true;
+} else {
+  this.isMobile = false;
+}
 } 
 -->
-  
-  <script>
+
+<script>
 export default {
   data() {
     return {
@@ -43,78 +43,78 @@ export default {
     this.checkMobile();
     window.addEventListener("resize", this.checkMobile);
   },
-
 };
 </script>
-  
-  <style lang="scss" scoped>
-header {
+
+<style lang="scss" scoped>
+.header {
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 1rem 0;
   background-color: white;
-}
+  margin-bottom: 20px;
 
-.logo {
-  font-size: 1.5rem;
-  font-weight: bold;
-}
+  &__logo {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
 
-.hamburger {
-  font-size: 1.5rem;
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-}
+  &__hamburger {
+    font-size: 1.5rem;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
 
-.menu-desktop {
-  display: flex;
-  a {
-    padding: 0.5rem;
-    text-decoration: none;
-    color: #333;
-    position: relative;
+  &__desktop {
+    border: 1px solid;
+    display: flex;
+    a {
+      padding: 0.5rem;
+      text-decoration: none;
+      color: #333;
+      position: relative;
 
-    &.router-link-exact-active {
-      font-weight: bold;
+      &.router-link-exact-active {
+        font-weight: bold;
 
-      &::after {
-        content: "";
-        bottom: -42px;
-        right: 5px;
-        position: absolute;
-        width: 45px;
-        height: 3px;
-        background-color: black;
+        &::after {
+          content: "";
+          bottom: -42px;
+          right: 5px;
+          position: absolute;
+          width: 45px;
+          height: 3px;
+          background-color: black;
+        }
+      }
+    }
+  }
+  &__mobile {
+    display: none;
+    flex-direction: column;
+    align-items: flex-end;
+    a {
+      padding: 0.5rem;
+      text-decoration: none;
+      color: #333;
+
+      &.router-link-exact-active {
+        font-weight: bold;
       }
     }
   }
 }
 
-.menu-mobile {
-  display: none;
-  flex-direction: column;
-  align-items: flex-end;
-  a {
-    padding: 0.5rem;
-    text-decoration: none;
-    color: #333;
-
-    &.router-link-exact-active {
-      font-weight: bold;
-    }
-  }
-}
-
 @media (max-width: 768px) {
-  .menu-desktop {
+  .header__desktop {
     display: none;
   }
 
-  .menu-mobile {
+  .header__mobile {
     display: flex;
   }
 }
+
 </style>
-  
